bbb94e36386c33344d5af0b5716dca4bc8c8595e
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-10-18 04:16:14 -0700

    My side noting Change

---
+++ src/Components/InputForm/UserInputForm.js
@@ -0,0 +1,57 @@
+import classes from "./InputForm.module.css";
+import React, { useState } from "react";
+import Modal from "../UI/Modal";
+import Button from "../UI/Button.js";
+export default function UserInputForm(props) {
+  const options = [' Matric Science(com)', 'Matric Science(bio)', 'Matric Arts', 'FSC(Pre Engineering)', 'FSC(Pre Medical)', 'ICS(Physics)', 'ICS(Stastics)', 'ICS(Economics)' , 'ICOM'];
+  const [name, setName] = useState("");
+  const [passedDegree, setpassedDegree] = useState("Matric Science(com)");
+  const [marks, setMarks] = useState("");
+  const inputNameHandler = (event) => {
+    setName(event.target.value);
+  };
+  const inputpassedDegreeHandler = (event) => {
+    setpassedDegree(event.target.value);
+  };
+  const inputMarksHandler = (event) => {
+    setMarks(event.target.value);
+  };
+  const submitForm = (event) => {
+    event.preventDefault();
+    const UserData = {
+      Name: name,
+      PassedDegree: passedDegree,
+      Marks: marks,
+    };
+    setName=
+    console.log(UserData);
+  };
+  return (
+    <Modal onClickFunc={props.onClose}>
+      <div className={classes.heading}>
+        <h3>Enter the Data</h3>
+      </div>
+      <form>
+        <label>Name</label>
+        <input type="text" value={name} onChange={inputNameHandler} />
+        <label>Passed Degree</label>
+        <select
+          onChange={inputpassedDegreeHandler}
+        >
+                    {options.map((option, index) => {
+                        return <option defaultValue='Matric Science(com)'
+                        value={options.value} key={index} >
+                            {option}
+                        </option>
+                    })}
+        </select>
+        <label>Your Marks</label>
+        <input type="text" value={marks} onChange={inputMarksHandler} />
+        <div className={classes.btnContainer}>
+          <Button onClickFunc={submitForm} btnTitle="Submit" />
+          <Button onClickFunc={props.onClose} btnTitle="Close" />
+        </div>
+      </form>
+    </Modal>
+  );
+}
